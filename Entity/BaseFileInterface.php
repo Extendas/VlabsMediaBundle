ebdbd65085553afe25dbf024f0bcc54d0e425ac8
Tayfun Aydin <tayfunaydin1996@gmail.com>
2023-03-13 14:03:55 +0100

    fix warning

--- Entity/BaseFileInterface.php
+++ Entity/BaseFileInterface.php
@@ -43,7 +43,7 @@ interface BaseFileInterface
     /**
      * Set File content type
      *
-     * @param string $name
+     * @param string $contentType
      */
     public function setContentType($contentType);
 
